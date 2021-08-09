2e09708685915313ae2c8442192f34fdf4bb77d4
Trevor Benyack <trevorbenyack@gmail.com>
2021-08-09 16:25:02 -0400

    completes video 41

--- public/socketStuff.js
+++ public/socketStuff.js
@@ -1,5 +1,17 @@
 let socket = io.connect('http://localhost:8080');
 
-socket.on('init', (data) => {
+// This function is called when the user clicks on the start button
+function init() {
+
+    // start drawing the screen
+    draw();
+
+    // call the init event when the client is ready for the data
+    socket.emit('init', {
+        playerName: player.name
+    })
+}
+
+socket.on('initReturn', (data) => {
     orbs = data.orbs;
 });
--- sockets/socketMain.js
+++ sockets/socketMain.js
@@ -7,6 +7,7 @@ const PlayerData = require('./classes/PlayerData');
 const PlayerConfig = require('./classes/PlayerConfig');
 const Orb = require('./classes/Orb');
 let orbs = [];
+let players = [];
 let settings = {
     defaultOrbs: 500,
     defaultSpeed: 6,
@@ -21,20 +22,26 @@ initGame();
 
 io.sockets.on('connect', (socket) => {
     // a player has connected
-    // maker a playerConfig object
-    let playerConfig = new PlayerConfig(settings);
+    socket.on('init', (data) => {
+        // maker a playerConfig object
+        let playerConfig = new PlayerConfig(settings);
 
-    // make a playerData object
-    // This is the data that everyone needs to know about everyone
-    let playerData = new PlayerData(null, settings);
+        // make a playerData object
+        // This is the data that everyone needs to know about everyone
+        let playerData = new PlayerData(data.playerName, settings);
 
-    // The server manages this one object
-    // make a master player object to hold both
-    let player = new Player(socket.id, playerConfig, playerData);
+        // The server manages this one object
+        // make a master player object to hold both
+        let player = new Player(socket.id, playerConfig, playerData);
 
-    socket.emit('init', {
-        orbs
-    })
+        socket.emit('initReturn', {
+            orbs
+        })
+
+        // This will add will new player onto the Players[]
+        players.push(playerData);
+    }); // end socket.on('init', ...)
+
 })
 
 // run at the beginning of a new game
